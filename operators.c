9594aac29c3d1e0ed9626b542d93e16fdf416b4b
Alex, Jr. A. Aparece <alexarnaizaparece@gmail.com>
2025-06-30 18:09:33 +0800

    Learned operators in C

---
+++ operators.c
@@ -0,0 +1,72 @@
+/* 
+Operators
+Operators are used to perform operations on variables and values.
+
+In the example below, we use the + operator to add together two values:
+
+Example
+int myNum = 100 + 50;
+
+
+
+Although the + operator is often used to add together two values, like in the example above, it can also be used to add together a variable and a value, or a variable and another variable:
+
+Example
+int sum1 = 100 + 50;        // 150 (100 + 50)
+int sum2 = sum1 + 250;      // 400 (150 + 250)
+int sum3 = sum2 + sum2;     // 800 (400 + 400)
+*/ 
+
+/* 
+C divides the operators into the following groups:
+
+Arithmetic operators
+Assignment operators
+Comparison operators
+Logical operators
+Bitwise operators
+*/ 
+
+
+/*
+Arithmetic Operators
+Arithmetic operators are used to perform common mathematical operations.
+
+Operator	Name	        Description	                            Example
++	        Addition	    Adds together two values    	        x + y	
+-	        Subtraction	    Subtracts one value from another	    x - y	
+*	        Multiplication	Multiplies two values           	    x * y	
+/	        Division	    Divides one value by another	        x / y	
+%	        Modulus	        Returns the division remainder  	    x % y	
+++	        Increment	    Increases the value of a variable by 1	++x	
+--	        Decrement	    Decreases the value of a variable by 1	--x	
+*/ 
+
+
+/*
+Assignment Operators
+Assignment operators are used to assign values to variables.
+
+In the example below, we use the assignment operator (=) to assign the value 10 to a variable called x:
+
+Example
+int x = 10;
+
+A list of all assignment operators:
+
+Operator	Example	    Same As	Try it
+=	        x = 5	    x = 5	
++=	        x += 3	    x = x + 3	
+-=	        x -= 3	    x = x - 3	
+*=      	x *= 3	    x = x * 3	
+/=      	x /= 3	    x = x / 3	
+%=	        x %= 3	    x = x % 3	
+&=	        x &= 3  	x = x & 3	
+|=      	x |= 3	    x = x | 3	
+^=	        x ^= 3	    x = x ^ 3	
+>>=	        x >>= 3	    x = x >> 3	
+<<=	        x <<= 3	    x = x << 3	
+*/ 
+
+// IM TIRED JUST SEARCH THE OTHERS 
+
