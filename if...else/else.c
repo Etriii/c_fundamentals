ec64b0186e81965be97962ab1dacecb139d18d2a
Alex, Jr. A. Aparece <alexarnaizaparece@gmail.com>
2025-06-30 18:26:07 +0800

    Learned else in C

---
+++ if...else/else.c
@@ -0,0 +1,19 @@
+/*
+The else Statement
+Use the else statement to specify a block of code to be executed if the condition is false.
+
+Syntax
+if (condition) {
+  // block of code to be executed if the condition is true
+} else {
+  // block of code to be executed if the condition is false
+}
+Example
+int time = 20;
+if (time < 18) {
+  printf("Good day.");
+} else {
+  printf("Good evening.");
+}
+// Outputs "Good evening."
+*/ 
