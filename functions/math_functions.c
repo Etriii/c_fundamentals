cda0b588f5d8781b87703a15c44990c9937a0e3f
Alex, Jr. A. Aparece <alexarnaizaparece@gmail.com>
2025-07-02 19:08:13 +0800

    Learned math functions in C

---
+++ functions/math_functions.c
@@ -0,0 +1,32 @@
+/*
+Math Functions
+There is also a list of math functions available, that allows you to perform mathematical tasks on numbers.
+
+To use them, you must include the math.h header file in your program:
+
+#include <math.h>
+Square Root
+To find the square root of a number, use the sqrt() function:
+
+Example
+printf("%f", sqrt(16));
+*/ 
+
+
+/*
+Round a Number
+The ceil() function rounds a number upwards to its nearest integer, and the floor() method rounds a number downwards to its nearest integer, and returns the result:
+
+Example
+printf("%f", ceil(1.4));
+printf("%f", floor(1.4));
+*/ 
+
+/*
+Power
+The pow() function returns the value of x to the power of y (xy):
+
+Example
+printf("%f", pow(4, 3));
+*/ 
+
